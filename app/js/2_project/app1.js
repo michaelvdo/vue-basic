711930ae040db4b12e560ad2534291b57a9cc216
Michael van den Oudenalder <michael.oudenalder@valtech.nl>
2017-08-01 14:05:25 +0200

    .

--- app/js/2_project/app1.js
+++ app/js/2_project/app1.js
@@ -1,16 +1,26 @@
 (function() {
   'use strict';
 
+  // component
+  const Child = {
+    props: ['parentMsg'],
+    template: '<span>{{ parentMsg }}</span>'
+  };
+
   // app
   if (document.getElementById('app-1')) {
     window.app = new Vue({
       el: '#app-1',
       data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        parentMsg: 'I\'m a child element!'
       },
       created: function() {
         console.log('app: First Vue app created!');
         console.log('This instance\'s message is:', this.message);
+      },
+      components: {
+        'child': Child
       }
     });
   }
